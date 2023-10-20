370a3a7b4a5870b2cb723f67f5561eb3488c7c94
dirkarnez <smalldirkalex@gmail.com>
2023-10-20 14:47:38 +0800

    - init, working

---
+++ main.c
@@ -0,0 +1,18 @@
+/*
+ * EIE3105_ATmega328P_Application.c
+ *
+ * Created: 13/4/2023 1:54:25 AM
+ * Author : home
+ */ 
+
+// #include <avr/io.h>
+
+
+int main(void)
+{
+    /* Replace with your application code */
+    while (1) 
+    {
+    }
+}
+
---
+++ setup.cmd
@@ -0,0 +1,3 @@
+avr8-toolchain
+http://packs.download.atmel.com/Atmel.ATmega_DFP.2.1.506.atpack
+x86_64-8.1.0-release-posix-seh-rt_v6-rev0

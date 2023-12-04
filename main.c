ca44ff70e4878d8d06b268a8ec663fcab6633e5c
Dirk Arnez <smalldirkalex@gmail.com>
2023-12-04 14:58:32 +0800

    - update main.c

--- main.c
+++ main.c
@@ -2,7 +2,7 @@
 #define F_CPU 16000000UL
 
 #define USART_BAUDRATE 9600
-#define UBRR_VALUE ((int)(ceil(((F_CPU / ((double)(USART_BAUDRATE) * (double)(16UL))) - (double)(1UL)))))
+#define UBRR_VALUE ((int)(ceil((((F_CPU) / ((double)(USART_BAUDRATE) * (double)(16UL))) - (double)(1UL)))))
 
 
 #include <avr/io.h>

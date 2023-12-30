200a65f5e2df8daef00024bee6cd0aeffcc974ee
dirkarnez <smalldirkalex@gmail.com>
2023-12-30 08:55:08 +0800

    - fix local-asm-build.cmd

--- local-asm-build.cmd
+++ local-asm-build.cmd
@@ -10,5 +10,7 @@ SET PATH=^
 %DOWNLOADS_DIR%\PortableGit\usr\bin;
 
 %DOWNLOADS_DIR%\x86_64-8.1.0-release-posix-seh-rt_v6-rev0\mingw64\bin\mingw32-make.exe clean
-avrasm2.exe -fI -o "main.hex"  -m "main.map"  -l "main.lss"  -S "main.tmp" -W+ie -I"%DOWNLOADS_DIR%\Downloads\Atmel.ATmega_DFP.2.1.506\avrasm\inc" -im328Pdef.inc -d "main.obj"  "main.asm"
+avrasm2.exe -fI -o "EIE3105_ATmega328P_Application.hex"  -m "EIE3105_ATmega328P_Application.map"  -l "EIE3105_ATmega328P_Application.lss"  -S "EIE3105_ATmega328P_Application.tmp" -W+ie -I"%DOWNLOADS_DIR%\Atmel.ATmega_DFP.2.1.506\avrasm\inc" -im328Pdef.inc -d "EIE3105_ATmega328P_Application.obj"  "main.asm"
 pause
+
+

f959ad6114b25a8cc4b29703c4a944613cf3cb66
Dirk Arnez <smalldirkalex@gmail.com>
2025-11-19 18:15:24 +0800

    - update main.c

--- main.c
+++ main.c
@@ -7,7 +7,23 @@
 #define NORMAL_MODE_VALUE(timer_bit, n_seconds, prescaler) ((int)(((1UL) << (timer_bit)) - ((n_seconds) * ((F_CPU) / (prescaler)))))
 #define CTC_MODE_VALUE(n_seconds, prescaler) ((int)(((n_seconds) * ((F_CPU) / (prescaler))) - (1UL)))
 
-#define UBRR_VALUE(UART_BAUDRATE) ((unsigned char)(((F_CPU)/((UART_BAUDRATE) * (16UL)))-((double)(1UL))))
+/* 
+https://github.com/arduino/ArduinoCore-avr/blob/87faf934a742fd6aa9fc269c99de5d529363f204/bootloaders/atmega/ATmegaBOOT_168.c#L375C1-L384C7
+
+#elif defined(__AVR_ATmega168__) || defined(__AVR_ATmega328P__) || defined (__AVR_ATmega328__)
+
+#ifdef DOUBLE_SPEED
+	UCSR0A = (1<<U2X0); //!!!!!!! Double speed mode USART0
+	UBRR0L = (uint8_t)(F_CPU/(BAUD_RATE*8L)-1);
+	UBRR0H = (F_CPU/(BAUD_RATE*8L)-1) >> 8;
+#else
+	UBRR0L = (uint8_t)(F_CPU/(BAUD_RATE*16L)-1);
+	UBRR0H = (F_CPU/(BAUD_RATE*16L)-1) >> 8;
+#endif
+*/
+
+#define UBRR_VALUE_LOW_SPEED(UART_BAUDRATE) ((unsigned char)(((F_CPU)/((UART_BAUDRATE) * (16UL)))-((double)(1UL))))
+#define UBRR_VALUE_DOUBLE_SPEED(UART_BAUDRATE) ((unsigned char)(((F_CPU)/((UART_BAUDRATE) * (8L)))-((double)(1UL))))
 
 #include <avr/io.h>
 

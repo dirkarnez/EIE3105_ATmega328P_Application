f394cbc61dcc4c0406bd8688b2e0a3eae9eee69f
Dirk Arnez <smalldirkalex@gmail.com>
2026-02-05 19:38:08 +0800

    - update local-build.cmd

--- local-build.cmd
+++ local-build.cmd
@@ -12,5 +12,5 @@ SET PATH=^
 
 
 %DOWNLOADS_DIR%\x86_64-8.1.0-release-posix-seh-rt_v6-rev0\mingw64\bin\mingw32-make.exe clean
-%DOWNLOADS_DIR%\x86_64-8.1.0-release-posix-seh-rt_v6-rev0\mingw64\bin\mingw32-make.exe all
+%DOWNLOADS_DIR%\x86_64-8.1.0-release-posix-seh-rt_v6-rev0\mingw64\bin\mingw32-make.exe VERBOSE=1 all
 pause
